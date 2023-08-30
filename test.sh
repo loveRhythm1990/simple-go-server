6caec5052a51ff0c409b7855dfffe68bb7707ee2
lr90 <qiuweimin@matrixorigin.cn>
2023-08-30 14:39:34 +0800

    fix

--- test.sh
+++ test.sh
@@ -2,22 +2,38 @@
 
 set -x -e
 
-tagList=$(curl -L -s 'https://hub.docker.com/v2/repositories/matrixorigin/matrixone/tags?page_size=1')
+echo "$@"
 
+replaceClusterVersion() {
+  echo "replaceClusterVersion args: " "$@"
+  clusterName=$1
+  newVersion=$2
+  sed -E -i s/"^(  version: nightly-[0-9a-z]{8})"/"  version: ${newVersion}"/ ./resources/"${clusterName}".yaml
+}
+
+if [ $# -ge 2 ]; then
+    replaceClusterVersion "$1" "$2"
+    exit 0
+fi
+
+
+# fetch latest version from docker hub
+tagList=$(curl -L -s 'https://hub.docker.com/v2/repositories/matrixorigin/matrixone/tags?page_size=1')
 lastUpdate=$(echo "${tagList}" | jq '.results[0].last_updated' | tr -d '"')
 lastUpdateSeconds=$(date +%s -d "${lastUpdate}")
 newTag=$(echo "${tagList}" | jq '.results[0].name' | tr -d '"')
 
 echo "${newTag}"
 
-halfday=43200
+# only update cluster if the fetched tag is updated in 12 hours
+halfDay=43200
 now=$(date +%s)
-diffSeconds=$((${now}-${lastUpdateSeconds}))
-if [ ${diffSeconds} > ${halfday} ]; then
+diffSeconds=$((now-lastUpdateSeconds))
+if [ ${diffSeconds} > ${halfDay} ]; then
         echo "latest image is 12 hours ago, skip updating"
-        #exit 0
+        exit 0
+else
+    replaceClusterVersion "freetier-01" "${newTag}"
 fi
-
-sed -E -i s/"^(  version: nightly-[0-9a-z]{8})"/"  version: ${newTag}"/ ./resources/freetier-01.yaml
 
 cat ./resources/freetier-01.yaml
